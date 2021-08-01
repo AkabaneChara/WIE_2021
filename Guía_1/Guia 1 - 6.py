b42ec9a637953fa9aac26457c8f40eb667142f0c
AkabaneChara <69637072+AkabaneChara@users.noreply.github.com>
2021-08-01 16:11:23 -0500

    Update Guia 1 - 6.py

--- Guía_1/Guia 1 - 6.py
+++ Guía_1/Guia 1 - 6.py
@@ -1,3 +1,7 @@
+#Escriba un código en el que se pida una cantidad entera de horas, a partir de las cuales debe
+#calcular el número de años, meses, semanas, días y horas que equivalen al número dado. Suponga
+#que los meses tienen exactamente 30 días e ignore los años bisiestos.
+
 hours = int(input("Ingrese El Número de Horas : "))
 
 print("Años : ", hours//8760)
