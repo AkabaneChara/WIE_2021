2a279373122079e872ecf8553b92cd05d609ce7a
AkabaneChara <69637072+AkabaneChara@users.noreply.github.com>
2021-08-01 16:49:17 -0500

    Update Guia 2 - 11.py

--- Guía_2/Guia 2 - 11.py
+++ Guía_2/Guia 2 - 11.py
@@ -1,3 +1,7 @@
+#Realice un programa que reciba 3 valores enteros correspondientes a los tres las longitudes
+#de los lados de un triángulo. A continuación, imprima si el triángulo es equilátero, isósceles
+#o escaleno.
+
 side_1 = input("Ingrese El Primer Lado Del Triangulo : ")
 side_2 = input("Ingrese El Segundo Lado Del Triangulo : ")
 side_3 = input("Ingrese El Tercer Lado Del Triangulo : ")
