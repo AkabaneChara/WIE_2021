0f1269dcc4b91e2f1f1ce1cda344a32bec3f5cda
AkabaneChara <69637072+AkabaneChara@users.noreply.github.com>
2021-08-01 16:08:52 -0500

    Update Guia 1 - 1.py

--- Guía_1/Guia 1 - 1.py
+++ Guía_1/Guia 1 - 1.py
@@ -1,3 +1,7 @@
+#Escriba un programa que le pida al usuario el nombre, los apellidos y la carrera. Posteriormente,
+#imprima un mensaje “Hola <nombre> <apellido>, Bienvenido a la carrera <carrera>”. Intente
+#imprimir el mismo mensaje de 3 maneras diferentes.
+
 name = input("Ingrese Su Nombre: ")
 last_name = input("Ingrese Su Apellido : ")
 career = input("Ingrese Su Carrera Universitaria : ")
