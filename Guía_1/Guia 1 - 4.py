82526a4c8db8f18efb88b7a8cc8d51e9323c5a28
AkabaneChara <69637072+AkabaneChara@users.noreply.github.com>
2021-08-01 16:10:18 -0500

    Update Guia 1 - 4.py

--- Guía_1/Guia 1 - 4.py
+++ Guía_1/Guia 1 - 4.py
@@ -1,3 +1,7 @@
+#Escriba un programa que pida al usuario su edad actual y un año entre 2022 y 2070. Calcule la
+#edad del usuario en el año dado e imprima “su edad en el año <año dado> será <años calculados>
+#años”.
+
 age = int(input("Ingrese Su Edad : "))
 year = int(input("Ingrese Un Año Entre 2022 y 2070 : "))
 
