811651b312718c99287ebec11cd18104448e59c1
AkabaneChara <69637072+AkabaneChara@users.noreply.github.com>
2021-08-01 16:41:47 -0500

    Update Guia 2 - 8.py

--- Guía_2/Guia 2 - 8.py
+++ Guía_2/Guia 2 - 8.py
@@ -1,3 +1,6 @@
+#Diseñe y pruebe un algoritmo que identifique si una cadena de texto es un palíndromo o no.
+#Entiéndase por palíndromo que se lee al derecho que al revés.
+
 word = input("Ingrese La Palabra A Comparar : ")
 word = word.lower()
 comparation = word[::-1]
